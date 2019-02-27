c760eab6acf53ca4f90c070e57ae86975f2097e5
Sean <w_angxin@qq.com>
2019-02-27 14:45:47 +0800

    track sdk 第一版

---
+++ lib/Tracker.framework/Headers/Tracker.h
@@ -0,0 +1,19 @@
+//
+//  Tracker.h
+//  Tracker
+//
+//  Created by Sean on 2019/2/12.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tracker.
+FOUNDATION_EXPORT double TrackerVersionNumber;
+
+//! Project version string for Tracker.
+FOUNDATION_EXPORT const unsigned char TrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tracker/PublicHeader.h>
+
+
